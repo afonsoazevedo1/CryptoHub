2f6ada841307d63082e0177533348358f1cb5e6c
Afonso Azevedo <afonsoazevedo42@gmail.com>
2026-08-22 21:48:55 -0300

    fix: remover arquivos inexistentes

--- app/src/main/java/com/example/cryptohub/domain/models/Exchange.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cryptohub.domain.models
-
-data class Exchange(
-    val id: Int,
-    val name: String,
-    val logo: String?,
-    val spotVolumeUsd: Double,
-    val dateLaunched: String?
-)
