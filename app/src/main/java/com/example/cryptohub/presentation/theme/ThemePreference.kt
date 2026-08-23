e43534297ced4394b78a9571db116c6740e04a30
Afonso Azevedo <afonsoazevedo42@gmail.com>
2026-08-22 21:44:37 -0300

    refactor: atualizar theme e themepreference

--- app/src/main/java/com/example/cryptohub/presentation/theme/ThemePreference.kt
+++ app/src/main/java/com/example/cryptohub/presentation/theme/ThemePreference.kt
@@ -3,22 +3,15 @@ package com.example.cryptohub.presentation.theme
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class ThemePreference(private val dataStore: DataStore<Preferences>) {
+class ThemePreference(dataStore: DataStore<Preferences>) {
     companion object {
         private val IS_DARK_MODE = booleanPreferencesKey("is_dark_mode")
     }
 
-    val isDarkModeFlow: Flow<Boolean> = dataStore.data.map { preferences ->
-        preferences[IS_DARK_MODE] ?: false
-    }
-
-    suspend fun setDarkMode(isDark: Boolean) {
-        dataStore.edit { preferences ->
-            preferences[IS_DARK_MODE] = isDark
-        }
+    val isDarkModeFlow: Flow<Boolean?> = dataStore.data.map { preferences ->
+        preferences[IS_DARK_MODE]
     }
 }
